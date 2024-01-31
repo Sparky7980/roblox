79d835c4772d1fb7ba6c513af3bf736906e61ef6
Sparky7980 <133839013+Sparky7980@users.noreply.github.com>
2024-01-31 08:58:06 -0800

    Update blanker.js

--- blanker.js
+++ blanker.js
@@ -6,7 +6,7 @@
             if (newWindow) {
                 // Write custom code to the new window
                 newWindow.document.write('<html><head><title>Essay</title></head><body>');
-                newWindow.document.write(' <iframe src="https://you-are-too-slow.onrender.com/" width="560" height="315"></iframe>');
+                newWindow.document.write(' <iframe src="https://you-are-too-slow.onrender.com/" width="1120" height="630"></iframe>');
                 newWindow.document.write('</body></html>');
                 newWindow.document.close(); // Close the document for writing
             } else {
