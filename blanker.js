cb865a5950b8520d027804c2586d6c9865fd24a9
Sparky7980 <133839013+Sparky7980@users.noreply.github.com>
2024-01-31 08:51:58 -0800

    Create blanker.js

---
+++ blanker.js
@@ -0,0 +1,15 @@
+      function openBlankPageWithCode() {
+            // Open a new window or tab with about:blank
+            var newWindow = window.open('about:blank', '_blank');
+
+            // Check if the new window is successfully opened
+            if (newWindow) {
+                // Write custom code to the new window
+                newWindow.document.write('<html><head><title>Essay</title></head><body>');
+                newWindow.document.write(' <iframe src="https://you-are-too-slow.onrender.com/" width="560" height="315"></iframe>');
+                newWindow.document.write('</body></html>');
+                newWindow.document.close(); // Close the document for writing
+            } else {
+                alert('Unable to open a new window. Please check your browser settings.');
+            }
+        }
