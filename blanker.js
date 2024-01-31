ca37a6d3d168e092e547cf5b6acec195b9078bb8
Sparky7980 <133839013+Sparky7980@users.noreply.github.com>
2024-01-31 15:03:11 -0800

    Update blanker.js

--- blanker.js
+++ blanker.js
@@ -5,9 +5,9 @@
             // Check if the new window is successfully opened
             if (newWindow) {
                 // Write custom code to the new window
-                newWindow.document.write('<html><head> <title>Khan Academy | Free Online Courses, Lessons &amp; Practice</title></head><link rel="shortcut icon" href="https://cdn.kastatic.org/images/favicon.ico?logo"><body>');
+                newWindow.document.write('<html><head> <link rel="icon" type="image/png" href="/imresizer-1706741917427.jpg"/><title>Khan Academy | Free Online Courses, Lessons &amp; Practice</title></head><link rel="shortcut icon" href="https://cdn.kastatic.org/images/favicon.ico?logo"><body>');
                 newWindow.document.write('<iframe allowfullscreen src="https://you-are-too-slow.onrender.com/" style="position:absolute; top:0px; left:0px; width:100%; height:100%; border: none; overflow: hidden;"></iframe>');
-                newWindow.document.write('</body></html>');
+                newWindow.document.write('');
                 newWindow.document.close(); // Close the document for writing
             } else {
                 alert('Unable to open a new window. Turn on popups.');
