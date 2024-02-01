aaf1cb989c7429ea57f10cbbf7cafdbeb976059a
Sparky7980 <133839013+Sparky7980@users.noreply.github.com>
2024-02-01 15:36:59 -0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function openGame() {
-    var iframe = document.getElementById("game-iframe");
-
-    // Toggle visibility of elements
-    iframe.style.display = "block";
-
-    // Set the iframe source
-    iframe.src = "https://you-are-too-slow.onrender.com/";
-}
